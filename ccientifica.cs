04c87c7b3860c43ea0dfc7797135ac3513478489
Lili <79230737+sailorlyx@users.noreply.github.com>
2022-10-10 19:27:01 +0200

    Add files via upload

--- ccientifica.cs
+++ ccientifica.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +8,18 @@ namespace ccientifica
 {
     internal class Program
     {
-        public static string Main(string[] args)
+        public static void Main(string[] args)
         {
-        return EjecutarCalcu();
+        EjecutarCalcu();
         }
 
-        public static double EjecutarCalcu()
+        public static void EjecutarCalcu()
         {
             Console.WriteLine("¡bienvenido a la calculadora cientifica! ¿qué operación te gustaría realizar? :D ");
-            return MenuCalcu();
+            MenuCalcu();
         }
 
-        public static double MenuCalcu()
+        public static void MenuCalcu()
         {
             
             Console.WriteLine("1. operaciones elementales (+, -, *, /) ");
@@ -27,40 +27,35 @@ namespace ccientifica
             
             switch(Console.ReadLine()) 
             {
-                case "1":
-                    return OperacionElem();
+               case "1":
+                    OperacionElem();
                     break;
 
 
-                case "2":
-                    Console.WriteLine("has elegido realizar otra operación");
+               case "2":
+                    OtrasOperaciones();
                     break;
 
                 default:
                     Console.WriteLine("tienes que elegir una de las dos opciones :(( ");
+                    MenuCalcu();
                     break;
             }
         }
 
-        public static double PedirNum()
+        public static double PedirNum(string peticion)
         {
-            double numerito1;
-            Console.WriteLine("dime un numerito");
-            numerito1 = Convert.ToDouble(Console.ReadLine());
-            return numerito1;
-
-            double numerito2;
-            Console.WriteLine("dime otro numerito :D");
-            numerito2 = Convert.ToDouble(Console.ReadLine());
-            return numerito2;
-
-
+            double numerito;
+            Console.WriteLine(peticion);
+            numerito = Convert.ToDouble(Console.ReadLine());
+            return numerito;
         }
 
-        public static double OperacionElem()
+        public static void OperacionElem()
         {
             Console.Write("¡has elegido hacer una operacion elemental! ");
-            return PedirNum();
+            double numerito1 = PedirNum("dime un numerito :D");
+            double numerito2 = PedirNum("dime otro numerito :P");
             Console.Write("introduce 'suma', 'resta', 'multiplicacion' o 'division' ");
 
             switch(Console.ReadLine())
@@ -77,8 +72,92 @@ namespace ccientifica
                 case "division":
                     Console.WriteLine("el total es " + (numerito1 / numerito2));
                     break;
+                default:
+                    Console.WriteLine("lo has hecho mal, vuelve a empezar porfi :( ");
+                    OperacionElem();
+                    break;
+            }
+        }
+
+        static void OtrasOperaciones()
+        {
+            Console.WriteLine("¡has elegido hacer otro tipo de operacion! ¿que tipo de operacion te gustaria hacer?");
+            Console.WriteLine("1. funciones trigonometricas");
+            Console.WriteLine("2. calcular valor absoluto");
+            Console.WriteLine("3. calcular con numero pi");
+            Console.WriteLine("4. calcular con exponentes");
+            Console.WriteLine("5. calcular logaritmo");
+
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    FuncionesTrigonometricas();
+                    break;
+                case "2":
+                    CalcularValorAbsoluto();
+                    break;
+                case "3":
+                    CalcularNumPi();
+                    break;
+            }
+
+
+        }
+
+        static void CalcularValorAbsoluto()
+        {
+           double numerito;
+           double valorAbs;
+
+           numerito = PedirNum("dime un numerito");
+           
+           if (numerito<0) {
+             valorAbs = numerito * -1;
+             Console.WriteLine("El valor absoluto de {0} es {1}" ,numerito, valorAbs);
+            } else {
+              valorAbs = numerito;
+             Console.WriteLine("El valor absoluto de {0} es {1}" ,numerito, valorAbs);
 
             }
+           
+        }
+
+        static void CalcularNumPi()
+        {
+            double numPi = 3.14159;
+            double numerito1;
+
+        }
+
+        static void FuncionesTrigonometricas()
+        {
+            Console.WriteLine("¿que funcion te gustaria hacer?");
+            Console.WriteLine("1.calcular seno");
+            Console.WriteLine("2.calcular coseno");
+            Console.WriteLine("3.calcular tangente");
+
+            switch(Console.ReadLine()) {
+                case "1":
+                    Console.WriteLine("has elegido calcular el seno");
+                    break;
+                case "2":
+                    Console.WriteLine("has elegido calcular el coseno");
+                    break;
+                case "3":
+                    Console.WriteLine("has elegido calcular la tangente");
+                    break;
+
+                default:
+                    Console.WriteLine("elige una de las opciones disponibles, porfi");
+                    FuncionesTrigonometricas();
+                    break;
+            }
+
+        }
+
+        static void CalcularSeno()
+        {
+            //me cago en mi puta vida
         }
 
      
